ecddfd5709e8b6254d7cb43b81729fa7298391eb
翻斗乐园 <ts1916767131@163.com>
2021-01-04 22:38:13 +0800

    BaseMapper封装完成

--- src/main/java/com/qy25/sm/mapper/SupplierMapper.java
+++ src/main/java/com/qy25/sm/mapper/SupplierMapper.java
@@ -1,11 +1,11 @@
 package com.qy25.sm.mapper;
 
 import com.qy25.sm.entity.Supplier;
-import com.qy25.sm.entity.SupplierExample;
-import com.qy25.sm.mapper.base.BaseMapper;
-import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
+import com.qy25.sm.mapper.base.BaseMapper;
+
+
+
 
 public interface SupplierMapper extends BaseMapper<Supplier,Long> {
 
